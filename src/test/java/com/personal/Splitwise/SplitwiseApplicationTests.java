9f0a034b706d61c00010315068376b745889a5a7
yash-manchanda <yashm99999@gmail.com>
2023-10-27 09:33:31 +0530

    project setup

---
+++ src/test/java/com/personal/Splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.Splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
